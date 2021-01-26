88a64cf5c57c151b9f3df2e3c274955839231b02
estebanmavarezb <emavarez@glamit.com.ar>
2021-01-26 15:40:09 -0300

    agrego nav

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import NavBar from './components/nav/navbar';
+
+function App() {
+  return (
+    <>
+      <NavBar />
+    </>
+  );
+}
+
+export default App;
